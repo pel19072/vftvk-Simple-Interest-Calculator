8fc6cab20fcc584f53dab46e75568c725c5b5e2c
pel19072 <68125920+pel19072@users.noreply.github.com>
2022-05-03 23:41:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function compute()
-{
+function compute(){
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;  
     var interest = principal * years * rate /100; 
     var year = new Date().getFullYear()+parseInt(years); 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+(principal+interest)+",\<br\>in the year "+year+"\<br\>"
 }
 
 function updateRate(){
